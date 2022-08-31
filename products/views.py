acfb4c073a3730dc29b6136cad93f82b97583c07
biandradee <emi.andrade37@hotmail.com>
2022-08-31 14:32:04 -0300

    create detail view in scr/products/views.py

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from django.shortcuts import render
 
 from .models import Product
@@ -12,3 +12,15 @@ class ProductListView(ListView):
       # context = super(ProductListView, self).get_context_data(*args, **kwargs)
       # print(context)
       # return context
+
+#Class Based View
+class ProductDetailView(DetailView):
+    #traz todos os produtos do banco de dados sem filtrar nada 
+    queryset = Product.objects.all()
+    template_name = "products/detail.html"
+    
+    #def get_context_data(self, *args, **kwargs):
+        #context = super(ProductDetailView, self).get_context_data(*args, **kwargs)
+        #print(context)
+        #return context
+
