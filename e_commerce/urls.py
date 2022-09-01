fc5ee2e4a276379eb540e752ef230c1d4e5280b7
biandradee <emi.andrade37@hotmail.com>
2022-09-01 16:32:40 -0300

    create featured- detail.html in scr/products/templates/products

--- e_commerce/urls.py
+++ e_commerce/urls.py
@@ -4,7 +4,10 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
-from products.views import ProductListView, ProductDetailView
+from products.views import (ProductListView, 
+                            ProductDetailView,
+                            ProductFeaturedListView,
+                            ProductFeaturedDetailView)
 
 from .views import home_page, about_page, contact_page, login_page, register_page
 
@@ -14,10 +17,13 @@ urlpatterns = [
 	path('contact/', contact_page),
         path('login/', login_page),
         path('register/', register_page),
+        path('featured/', ProductFeaturedListView.as_view()),
+        path('featured/<int:pk>/', ProductFeaturedDetailView.as_view()),
         path('products/', ProductListView.as_view()),
-        path('products/<int:pk>', ProductDetailView.as_view()),
-	path('admin/', admin.site.urls),
-]
+        path('products/<int:pk>/', ProductDetailView.as_view()),
+	path('admin/', admin.site.urls)
+        ]
+
 if settings.DEBUG:
     urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
