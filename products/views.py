3ec8a0a99551466ae05005026d819523391ffc26
biandradee <emi.andrade37@hotmail.com>
2022-09-01 15:55:55 -0300

    create ProductQueryset class and define active and featured method inside this class, extend get_querysetmethod and featured method inside ProductManager class in scr/products/models.py

--- products/views.py
+++ products/views.py
@@ -1,22 +1,24 @@
+from django.http import Http404
 from django.views.generic import ListView, DetailView
 from django.shortcuts import render, get_object_or_404
 
 from .models import Product
 
+#Class Based View
 class ProductListView(ListView):
     #traz todos os produtos do banco de dados sem filtrar nada 
     queryset = Product.objects.all()
     template_name = "products/list.html"
-    
-    #def get_context_data(self, *args, **kwargs):
-      # context = super(ProductListView, self).get_context_data(*args, **kwargs)
-      # print(context)
-      # return context
+
+    # def get_context_data(self, *args, **kwargs):
+    #     context = super(ProductListView, self).get_context_data(*args, **kwargs)
+    #     print(context)
+    #     return context
+
 
 #Class Based View
 class ProductDetailView(DetailView):
     #traz todos os produtos do banco de dados sem filtrar nada 
-    queryset = Product.objects.all()
     template_name = "products/detail.html"
     
     def get_context_data(self, *args, **kwargs):
@@ -24,3 +26,9 @@ class ProductDetailView(DetailView):
       print(context)
       return context
 
+    def get_object(self, *args, **kwargs):
+      pk = self.kwargs.get('pk')
+      instance = Product.objects.get_by_id(pk)
+      if instance is None:
+        raise Http404("Esse produto não existe!")
+      return instance
